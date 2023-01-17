b0f4d79bb53ea3442bc5e6a88dca870b3262c3f4
Yoman666 <ivy410384@gmail.com>
2023-01-17 22:20:42 +0800

    1020

--- js/products.js
+++ js/products.js
@@ -1,4 +1,6 @@
 import { createApp } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js';
+
+// 1初始化modal 
 let productModal = '';
 let delProductModal='';
 createApp({
@@ -6,15 +8,18 @@ createApp({
         return {
             apiUrl:"https://vue3-course-api.hexschool.io/v2",
             path:"yoyo123456",
+            // 放全部商品的陣列
             products:[],
+            // 單一商品的放置位置
             temproduct:{
                 imagesUrl:[],
             },
+            // 判斷是新建的產品或是已有的產品，用來判斷要乎要哪種api
             isNew: false,
         }
     },
     mounted() {
-        //modal init 綁定
+        // 2modal init 綁定(固定格式)
         productModal = new bootstrap.Modal(document.getElementById('productModal'), {
             keyboard: false
         });
@@ -26,46 +31,51 @@ createApp({
         const token = document.cookie.replace(/(?:(?:^|.*;\s*)hexToken\s*=\s*([^;]*).*$)|^.*$/, '$1');
         axios.defaults.headers.common.Authorization = token;
 
+        // 需要先呼叫checkin 開始判斷
         this.checkin();
     },
     methods: {
         checkin(){
             const api = `${this.apiUrl}/api/user/check`;
             axios.post(api).then(()=>{
-                // 跨區要加this
+                //跨區要加this
+                //checkin成功 > show出所有產品
                 this.showProducts();
-
             }).catch((err)=>{
                 window.location='index.html';
             })
         },
         //取出商品資料放置在自訂的陣列中
         //要怎麼知道取出的位置?console.log(response);
-        // console.log(this.products);
-
+        // console.log(this.products); > 裡面看
         showProducts(){
             const api = `${this.apiUrl}/api/${this.path}/admin/products/all`
             axios.get(api).then((response)=>{
-                
                 this.products = response.data.products;
             }).catch((err)=>{
                 alert(err.response.data.message);
             })
         },
         
+        //post >新建產品所用的api方法
+        //put >更新商品所用的api方法
+        //這裡使用let是因為api 跟 http 在這個地方會更改
         updateProduct(){
             let api = `${this.apiUrl}/api/${this.path}/admin/product`;
             let http ='post';
 
+            // 如果是舊的商品之行這段
             if(!this.isNew){
                 api = `${this.apiUrl}/api/${this.path}/admin/product/${this.temproduct.id}`;
                 http = 'put';
             }
 
-            
+            //這裡的{data:}是資料裡規定
             axios[http](api,{data: this.temproduct}).then((response)=>{
-                alert('update sucess');
+                alert('%%%%%%更新成功花拉拉~~~你好棒');
+                // 按下確認更新成功後要把Modal關掉
                 productModal.hide();
+                // 更新完後需要再渲染一次showproducts
                 this.showProducts();
             }).catch((err)=>{
                 alert(err.response.data.message);
@@ -76,14 +86,19 @@ createApp({
             const api = `${this.apiUrl}/api/${this.path}/admin/product/${this.temproduct.id}`;
 
             axios.delete(api).then((reapose)=>{
-                alert('delet success');
+                // 按下確認，要把Modal關掉
                 delProductModal.hide();
+                // 需要再渲染一次反正每次增刪改後都要再次渲染一次資料
                 this.showProducts();
             }).catch((err)=>{
                 alert(err.response.data.message);
             })
         },
 
+        // 先判斷是哪一類型的資料更動
+        // new > 要將temproduct再次初始化，不然會有上一個的資料內容 > isNew狀態改成true這會影響到api的選擇 >芝麻打開Modal開始新增產品
+        // edit >將前台for迴圈的item帶入，並賦予值給temproduct > isNew狀態改成false
+        // del >
         showModal(isNew, item){
             if(isNew === 'new'){
                 this.temproduct = {
@@ -97,7 +112,7 @@ createApp({
                 productModal.show();
             } else if (isNew==='del'){
                 //important要加這段才可以取得產品的id 才能知道要刪除哪一個
-                this.temproduct = {...item};
+                // this.temproduct = {...item};
                 delProductModal.show();
             }
         },
